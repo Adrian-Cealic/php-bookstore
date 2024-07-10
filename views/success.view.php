9a432ca916a3477a1b41af631e5c1f10db0eab78
Adrian-Cealic <adrian.cealic@gmail.com>
2024-07-10 22:18:18 +0300

    refacterd the code

--- views/success.view.php
+++ views/success.view.php
@@ -16,47 +16,63 @@
             <div class="flex gap-2">
                 <span>"avatar"</span>
                 <div class="flex flex-col items-start">
-                    <span class="font-semibold"><?php if(isset($_GET['username'])){
-                        echo htmlspecialchars($_GET['username']) ;
+                    <span class="font-semibold">
+                        <?php if (isset($_GET['username'])) {
+                            echo htmlspecialchars($_GET['username']);
                         }
-                        ?></span>
-                    <span><?php if(isset($_GET['email'])){
-                        echo htmlspecialchars($_GET['email']) ;
+                        ?>
+                    </span>
+                    <span>
+                        <?php if (isset($_GET['email'])) {
+                            echo htmlspecialchars($_GET['email']);
                         }
-                        ?> </span>
+                        ?>
+                    </span>
                 </div>
             </div>
             <span class="border border-gray-500 w-full"></span>
             <div class="flex justify-between w-full px-8">
                 <span class="font-semibold">Name</span>
-                <span name="namebox"><?php if(isset($_GET['username'])){
-                        echo htmlspecialchars($_GET['username']) ;
-                        }
-                        ?></span>
+                <span name="namebox">
+                    <?php
+                    if (isset($_GET['username'])) {
+                        echo htmlspecialchars($_GET['username']);
+                    }
+                    ?>
+                </span>
             </div>
             <span class="border border-gray-500 w-full"></span>
             <div class="flex justify-between w-full px-8">
                 <span class="font-semibold">Email account</span>
-                <span name="emailbox"><?php if(isset($_GET['email'])){
-                        echo htmlspecialchars($_GET['email']) ;
-                        }
-                        ?></span>
+                <span name="emailbox">
+                    <?php
+                    if (isset($_GET['email'])) {
+                        echo htmlspecialchars($_GET['email']);
+                    }
+                    ?>
+                </span>
             </div>
             <span class="border border-gray-500 w-full"></span>
             <div class="flex justify-between w-full px-8">
                 <span class="font-semibold">Mobile number</span>
-                <span name="numberbox"><?php if(isset($_GET['email'])){
-                        echo htmlspecialchars($_GET['telefon']) ;
-                        }
-                        ?></span>
+                <span name="numberbox">
+                    <?php
+                    if (isset($_GET['email'])) {
+                        echo htmlspecialchars($_GET['telefon']);
+                    }
+                    ?>
+                </span>
             </div>
             <span class="border border-gray-500 w-full"></span>
             <div class="flex justify-between w-full px-8">
                 <span class="font-semibold">Location</span>
-                <span name="locationbox"><?php if(isset($_GET['email'])){
-                        echo htmlspecialchars($_GET['locatie']) ;
-                        }
-                        ?></span>
+                <span name="locationbox">
+                    <?php
+                    if (isset($_GET['email'])) {
+                        echo htmlspecialchars($_GET['locatie']);
+                    }
+                    ?>
+                </span>
             </div>
         </div>
     </form>
